30610583174ca0ae1bf295e4b2437be8c7aa61a5
pixcelo <bodomu@gmail.com>
2023-02-04 06:41:12 +0900

    リファクタリング

--- fetcher.py
+++ fetcher.py
@@ -2,40 +2,15 @@ import asyncio
 import pybotters
 import pandas as pd
 from loguru import logger
-
-import json
-import requests
 import pandas as pd
 import datetime
-import time
-
-# async def main():
-#     async with pybotters.Client() as client:
-#         path = generate_path(
-#             exchange='bitflyer',
-#             pair='btcjpy',
-#             min='60',
-#             unixTime=1675368565
-#             )
-#         # REST API
-#         resp = await client.get(path)
-#         data = await resp.json()
-#         print(resp)
-#         # print(data)
-#         # df = pd.read_json(data)
-#         # df
-
-#         # メインループ
-#         # while True:
-#         #     # データ参照
-#         #     orderbook = store.orderbook.find()
 
 async def main(candle: int, before: int, size: int) -> pd.DataFrame:
     async with pybotters.Client() as client:
         url="https://api.cryptowat.ch/markets/bitflyer/btcfxjpy/ohlc?periods="
         candle=candle*60
         period=("%s"%(candle))
-        period=[period]
+        # period=[period]
         beforeurl="&before="
         afterurl="&after="
         # The maximum size of data is 6000 depending on a timing (sometimes you get less than 6000)
@@ -46,18 +21,12 @@ async def main(candle: int, before: int, size: int) -> pd.DataFrame:
         response = await client.get(URL)
         body = await response.json()
         res = body['result']
-        # print(data['result'])
-
-        # res=json.loads(requests.get(URL).text)["result"]
-        # print(res)
         
         data = []
-        for i in period:
-            row = res[i]
-            for column in row:
-                if column[4] != 0:
-                    column = column[0:6]
-                    data.append(column)
+        for column in res[period]:
+            if column[4] != 0:
+                column = column[0:6]
+                data.append(column)
 
         date = [price[0] for price in data]
         op = [int(price[1]) for price in data]
